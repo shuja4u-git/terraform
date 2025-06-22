fb2652a74fcd322d50eacad52dcb5f5b25d46183
shuja-git <shuja.devops@gmail.com>
2025-06-22 18:42:47 +0530

    instance name fix

--- terraform-multi-region/main.tf
+++ terraform-multi-region/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instance" {
+resource "aws_instance1" "instance" {
   ami           = var.dev-ami
   instance_type = var.dev-instance_type
   provider      = aws.east1
@@ -7,7 +7,7 @@ resource "aws_instance" "instance" {
     Name = "terraform-instance1"
   }
 }
-resource "aws_instance" "instance" {
+resource "aws_instance2" "instance" {
   provider      = aws.east2
   ami           = var.stg-ami
   instance_type = var.stg-instance_type
